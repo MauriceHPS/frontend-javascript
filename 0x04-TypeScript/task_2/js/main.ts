39202a39ddc04059c3ea7facdf6ca52ff19dd30d
MauriceHPS <maurice@headpoint.info>
2025-11-10 15:41:03 +0300

    Created functions specific to employees.

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,69 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): String;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+
+  d = new Director();
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): String {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+
+  t = new Teacher();
+}
+
+function createEmployee(salary: string | number) {
+  if (typeof salary === "number") {
+    if (salary < 500) {
+      return new Teacher();
+    } else {
+      return new Director();
+    }
+  } else {
+    return new Director();
+  }
+}
+
+function isDirector(employee: string) {}
+
+function executeWork(employee: string) {
+  if (employee === "Director") return new Director().workDirectorTasks();
+
+  if (employee === "Teacher") return new Teacher().workTeacherTasks();
+}
+
+console.log(createEmployee(500));
+
+console.log(createEmployee(700));
+
+console.log(createEmployee(100));
